643eefa2b5074ef940b7c1ceaabc3bbb10f00efc
Nuno Rolo <1900405@estudante.uab.pt>
2021-04-24 12:31:50 +0100

    Esqueleto inicial

---
+++ Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheewbacca_PDFSharp
+{
+    class Model{
+
+        private View view;
+
+        public Model(View v) {
+            view = v;
+        }
+
+        public void ProcessarDados() { 
+            //Processar os dados
+        
+        }
+
+        public void SolicitaAlteracoes() { 
+            //Solicitação das Alterações
+        
+        }
+
+        public void EfetuarProcessamento() { 
+            //Realiza o processamento
+        
+        }
+
+        public void Destruir() { 
+            //Destroi 
+        }
+    }
+}
