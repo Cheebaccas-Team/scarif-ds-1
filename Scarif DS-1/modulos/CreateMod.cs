4b9fb1fd8dee0206ae02617bbb1388fe83e9efb6
João Pinto <joaopedrodpinto@gmail.com>
2021-05-26 11:50:01 +0100

    Split Final

--- Scarif DS-1/modulos/CreateMod.cs
+++ Scarif DS-1/modulos/CreateMod.cs
@@ -13,14 +13,6 @@ namespace Scarif_DS_1.modulos
     {
 
         public static void SplitDocument(Model modelo)
-
-        /*{
-           //Dado um ficheiro com n páginas devolve n ficheiros com 1 página na path de saída
-           Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-           // Open the file
-           PdfDocument inputDocument = PdfReader.Open(Path.Combine(filePath, filename), PdfDocumentOpenMode.Import);
-       */
         {
             try
             {
@@ -111,23 +103,6 @@ namespace Scarif_DS_1.modulos
             {
                 throw new DllNotFoundException(erro.Message);
 
-                /*
-                                string name = Path.GetFileNameWithoutExtension(filename);
-
-                                for (int idx = 0; idx < inputDocument.PageCount; idx++)
-                                {
-
-                                    // Create new document
-                                    PdfDocument outputDocument = new PdfDocument();
-                                    outputDocument.Version = inputDocument.Version;
-                                    outputDocument.Info.Title = String.Format("Page {0} of {1}", idx + 1, inputDocument.Info.Title);
-                                    outputDocument.Info.Creator = inputDocument.Info.Creator;
-
-                                    // Add the page and save it
-                                    outputDocument.AddPage(inputDocument.Pages[idx]);
-                                    outputDocument.Save(Path.Combine(outputPath,
-                                        String.Format("{0} - Page {1}_tempfile.pdf", name, idx + 1)));
-                                    */
             }
             } } 
 
